1a961cde76b40846da87a5a56375f7e2a0f26d6a
Karol Skolasiński <karol.skolasinski@7willows.com>
2025-06-22 11:03:47 +0200

    feat: close hamburger

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerCheckbox = document.getElementById("hamburger");
+  const menuItems = document.querySelectorAll(
+    "#hamburger ~ ul a, #hamburger ~ ul button, #hamburger ~ ul li > a",
+  );
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (hamburgerCheckbox.checked) {
+        hamburgerCheckbox.checked = false;
+      }
+    });
+  });
+});
